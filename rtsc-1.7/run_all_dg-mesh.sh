251f6080d0e815ba0254389f4881610d456e20d2
Changwoon Choi <changwoon.choi00@gmail.com>
2024-11-11 18:55:22 +0900

    echo -> actual script

--- rtsc-1.7/run_all_dg-mesh.sh
+++ rtsc-1.7/run_all_dg-mesh.sh
@@ -2,5 +2,5 @@
 
 for i in {0..99}
 do
-    echo ./rtsc ../data/dystroke/robin/dynamic_mesh/frame_$(printf "%d" $i).ply ../data/dystroke/robin/predict/$(printf "%d" $i).txt ../output/dystroke/robin/predict/$(printf "%d" $i).ppm
+    ./rtsc ../data/dystroke/robin/dynamic_mesh/frame_$(printf "%d" $i).ply ../data/dystroke/robin/predict/$(printf "%d" $i).txt ../output/dystroke/robin/predict/$(printf "%d" $i).ppm
 done
